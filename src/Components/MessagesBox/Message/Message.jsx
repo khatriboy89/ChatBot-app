f7c3ab4fb813c94663966ffb2262970f94bc76b8
rahul vedi <rahul.v7800@gmail.com>
2021-06-09 11:22:18 +0530

    change my name to me in Message.jsx

--- src/Components/MessagesBox/Message/Message.jsx
+++ src/Components/MessagesBox/Message/Message.jsx
@@ -17,7 +17,7 @@ const Message = ({ message: { text, user }, name }) => {
     isSentByCurrentUser
       ? (
         <div className="messageContainer justifyEnd">
-          <p className="sentText pr-10">{trimmedName}</p>
+          <p className="sentText pr-10">Me</p>
           <div className="messageBox backgroundBlue">
             <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
           </div>
