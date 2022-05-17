4997de9d16c6b3986bbd37b0a5495d6214c0a7c3
Rahul Vedi <rahul.v7800@gmail.com>
2022-05-17 14:25:50 +0530

    fix refresh problem

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 import Join from './Components/Join/Join'
 import Chat from './Components/Chat/Chat'
-import './app.css'
+import './App.css'
 const app = () => {
     return(
         <Router>
